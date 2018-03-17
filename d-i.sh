75b309309731c388776c59572bc1b05aa9235186
Sean Ho <git@seanho.com>
2018-03-17 05:14:31 -0700

    put user-configurable settings at top

--- d-i.sh
+++ d-i.sh
@@ -2,10 +2,11 @@
 # wget -O- https://d-i.seanho.com/d-i.sh | bash -
 ## Configure grub for auto-install of Debian from ISO
 
-repo="https://github.com/ho-ansible/d-i"
 ver="9.4.0"
 arch="amd64"
 iso="https://cdimage.debian.org/debian-cd/current/$arch/iso-cd/debian-$ver-$arch-netinst.iso"
+
+repo="https://github.com/ho-ansible/d-i"
 
 cd /boot
 wget -N "$iso"
