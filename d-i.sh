e1324abc787bdd58ff8917999c95e719a65a8d0d
Sean Ho <git@seanho.com>
2018-03-26 16:41:37 -0700

    use netinstall initrd, not cdimage

--- d-i.sh
+++ d-i.sh
@@ -1,17 +1,19 @@
 #! /bin/sh
 # wget -O- https://d-i.seanho.com/d-i.sh | bash -
-## Configure grub for auto-install of Debian from ISO
+## Configure grub for Debian netinstall
 
-ver="9.4.0"
 arch="amd64"
-iso="https://cdimage.debian.org/debian-cd/current/$arch/iso-cd/debian-$ver-$arch-netinst.iso"
+url="http://ftp.debian.org/debian/dists/stable/main/installer-$arch/current/images/netboot/debian-installer/$arch"
 
 repo="https://github.com/ho-ansible/d-i"
 
 cd /boot
-wget -N "$iso"
 git clone --depth 1 $repo
 cd $(basename $repo)
+
+for file in linux initrd.gz; do
+  wget "$url/$file"
+done
 
 install -Cv -o root -g root -m 755 grub.d/* /etc/grub.d/
 update-grub
