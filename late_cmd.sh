c8f5f87c0e11496f0a7a76ca70c0b0a5f4a68761
Sean Ho <git@seanho.com>
2018-04-20 21:52:00 -0700

    quote vars

--- late_cmd.sh
+++ late_cmd.sh
@@ -1,17 +1,16 @@
 #! /bin/sh
-## Extra commands after fresh Debian install,
-## before first boot
+## Run after fresh Debian install, before first boot
 ## See preseed/late_command
 
 ## SSH pubkeys
 keys_url="https://f.seanho.com/vps/keys-rsa"
 keys_tmp="/tmp/authorized_keys"
 
-wget -qO $keys_tmp "$keys_url"
+wget -qO "$keys_tmp" "$keys_url"
 
 mkdir -vpm 700 /root/.ssh
-install -vDm 600 $keys_tmp /root/.ssh/
-install -vDm 600 $keys_tmp /etc/dropbear-initramfs/
+install -vDm 600 "$keys_tmp" /root/.ssh/
+install -vDm 600 "$keys_tmp" /etc/dropbear-initramfs/
 
 ## SSH server (dropbear) config
 echo "DROPBEAR_OPTIONS=\"-p 26 -s\"" >> /etc/dropbear-initramfs/config
